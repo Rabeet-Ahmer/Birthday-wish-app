b6243e3d2008167819cbbb0d5a2a693a153fc9a4
Rabeet-Ahmer <rabeetahmer9749@gmail.com>
2024-10-22 18:15:17 +0500

    added-file

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BirthdayWish from '@/components/birthday-wish'
+
+const page = () => {
+  return (
+    <div>
+      <BirthdayWish/>
+    </div>
+  )
+}
+
+export default page
